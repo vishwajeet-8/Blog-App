b6251e8f094216b7e68402ad11a087ff114fd701
vishwajeet-8 <vishwajeetrout8@gmail.com>
2024-08-17 23:24:26 +0530

    forntend designed for blog app

---
+++ client/src/App.jsx
@@ -0,0 +1,65 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Single from "./pages/Single.jsx";
+import Write from "./pages/Write.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import "./style.scss";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const posts = [
+    {
+      id: 1,
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sint.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab animi possimus molestias culpa itaque, voluptatibus libero accusamus necessitatibus quos facilis! Totam fugit mollitia nisi iure accusamus, nesciunt eveniet. Temporibus, nulla.",
+      img: "https://images.pexels.com/photos/863988/pexels-photo-863988.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 2,
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sint.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab animi possimus molestias culpa itaque, voluptatibus libero accusamus necessitatibus quos facilis! Totam fugit mollitia nisi iure accusamus, nesciunt eveniet. Temporibus, nulla.",
+      img: "https://images.pexels.com/photos/256262/pexels-photo-256262.jpeg",
+    },
+    {
+      id: 3,
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sint.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab animi possimus molestias culpa itaque, voluptatibus libero accusamus necessitatibus quos facilis! Totam fugit mollitia nisi iure accusamus, nesciunt eveniet. Temporibus, nulla.",
+      img: "https://images.pexels.com/photos/962312/pexels-photo-962312.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id: 4,
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sint.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab animi possimus molestias culpa itaque, voluptatibus libero accusamus necessitatibus quos facilis! Totam fugit mollitia nisi iure accusamus, nesciunt eveniet. Temporibus, nulla.",
+      img: "https://images.pexels.com/photos/274937/pexels-photo-274937.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id: 5,
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sint.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab animi possimus molestias culpa itaque, voluptatibus libero accusamus necessitatibus quos facilis! Totam fugit mollitia nisi iure accusamus, nesciunt eveniet. Temporibus, nulla.",
+      img: "https://images.pexels.com/photos/256381/pexels-photo-256381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 6,
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sint.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab animi possimus molestias culpa itaque, voluptatibus libero accusamus necessitatibus quos facilis! Totam fugit mollitia nisi iure accusamus, nesciunt eveniet. Temporibus, nulla.",
+      img: "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
+
+  return (
+    <div className="home">
+      <div className="posts">
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={post.img} alt="" />
+            </div>
+            <div className="content">
+              <Link to={`/post/${post.id}`} className="link">
+                <h1>{post.title}</h1>
+              </Link>
+              <p>{post.desc}</p>
+              <button>Read More</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
